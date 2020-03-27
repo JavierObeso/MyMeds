ca0d615eaaeae6cff818c2ef90f4e3e4f431f61e
Javier Obeso <halojavi@hotmail.com>
2020-03-26 23:36:40 -0700

    Pantalla principal

--- app/src/main/java/javier/obeso/mymeds/MainActivity.kt
+++ app/src/main/java/javier/obeso/mymeds/MainActivity.kt
@@ -2,11 +2,19 @@ package javier.obeso.mymeds
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
+import java.text.SimpleDateFormat
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val fecha_actual = SimpleDateFormat("EEEE dd 'de' MMMM yyyy", Locale("es","ES")).format(Date())
+
+        val fecha:TextView = findViewById(R.id.fecha) as TextView
+        fecha.setText(""+fecha_actual.capitalize()+"")
     }
 }
