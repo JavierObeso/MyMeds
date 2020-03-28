1d06b5873f673784968d70a7630424e686ae9236
Javier Obeso <halojavi@hotmail.com>
2020-03-27 21:56:25 -0700

    Avance pantalla principal

--- app/src/main/java/javier/obeso/mymeds/MainActivity.kt
+++ app/src/main/java/javier/obeso/mymeds/MainActivity.kt
@@ -1,12 +1,26 @@
 package javier.obeso.mymeds
 
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.BaseAdapter
+import android.widget.TextClock
 import android.widget.TextView
+import javier.obeso.mymeds.entidades.Alarma
+import kotlinx.android.synthetic.main.activity_main.*
 import java.text.SimpleDateFormat
 import java.util.*
 
 class MainActivity : AppCompatActivity() {
+
+    private var adaptador: AdaptadorAlarmas? = null
+
+    companion object{
+        var alarmas = ArrayList<Alarma>()
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -16,5 +30,38 @@ class MainActivity : AppCompatActivity() {
 
         val fecha:TextView = findViewById(R.id.fecha) as TextView
         fecha.setText(""+fecha_actual.capitalize()+"")
+
+        adaptador = AdaptadorAlarmas(this, alarmas)
+        lista.adapter = adaptador
+    }
+
+    private class AdaptadorAlarmas: BaseAdapter {
+        var alarmas = ArrayList<Alarma>()
+        var contexto: Context? = null
+
+        constructor(contexto: Context, tasks: ArrayList<Alarma>){
+            this.contexto = contexto
+            this.alarmas = tasks
+        }
+
+        override fun getView(p0: Int, p1: View?, p2: ViewGroup?): View {
+            var alarma = alarmas[p0]
+            var inflador = LayoutInflater.from(contexto)
+            var vista = inflador.inflate(R.layout, null)
+
+            return vista
+        }
+
+        override fun getItem(p0: Int): Any {
+            return alarmas[p0]
+        }
+
+        override fun getItemId(p0: Int): Long {
+            return p0.toLong()
+        }
+
+        override fun getCount(): Int {
+            return alarmas.size
+        }
     }
 }
---
+++ app/src/main/java/javier/obeso/mymeds/entidades/alarma.kt
@@ -0,0 +1,3 @@
+package javier.obeso.mymeds.entidades
+
+data class Alarma (var tipo: String, var frecuencia: ArrayList<String>, var dosis: String, var hora: String)
