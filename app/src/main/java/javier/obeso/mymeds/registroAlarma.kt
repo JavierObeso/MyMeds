d1026419c71d36b8d271ce0393c2d03acd77323a
jonatanyael <jycc1207@gmail.com>
2020-04-01 21:21:47 -0700

    Activities registro de medicamento y registro de alarma

--- app/src/main/java/javier/obeso/mymeds/registroAlarma.kt
+++ app/src/main/java/javier/obeso/mymeds/registroAlarma.kt
@@ -1,13 +1,21 @@
 package javier.obeso.mymeds
 
+import android.app.DatePickerDialog
+import android.app.TimePickerDialog
 import android.os.Bundle
+import android.text.Editable
+import android.text.InputType
 import android.view.View
 import android.widget.*
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.content.res.ResourcesCompat
+import java.text.SimpleDateFormat
+import java.util.*
 
 
 class registroAlarma : AppCompatActivity() {
+
+    val timeSetListener = null;
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -16,7 +24,9 @@ class registroAlarma : AppCompatActivity() {
         configurarSpinnerMedicamentos()
         configurarSpinnerFrecuencia()
         configurarSpinnerDosis()
-        configurarSpinnerHora()
+        configurarEditTextHora()
+        configurarEditTextInicio()
+        configurarEditTextFin()
         configurarSpinnerRevisor()
 
     }//Termina metodo onCreate()
@@ -96,25 +106,55 @@ class registroAlarma : AppCompatActivity() {
             }
         }
     }
-    fun configurarSpinnerHora() {
-        val horas = resources.getStringArray(R.array.arreglo_hora)
+    fun configurarEditTextHora() {
+        val editTextHora = findViewById(R.id.et_hora) as EditText
+        editTextHora.setRawInputType(InputType.TYPE_NULL)
+        val cal = Calendar.getInstance()
+        editTextHora.setOnClickListener {
+            val timeSetListener = TimePickerDialog.OnTimeSetListener { timePicker, hour, minute ->
+                cal.set(Calendar.HOUR_OF_DAY, hour)
+                cal.set(Calendar.MINUTE, minute)
 
-        val spinner = findViewById<Spinner>(R.id.spinner_hora)
-        if (spinner != null) {
-            //val adapter = ArrayAdapter(this, android.R.layout.simple_spinner_item, medicamentos)
-            val adapter = ArrayAdapter(this, R.layout.spinner_item, horas)
-            adapter.setDropDownViewResource(R.layout.spinner_dropdown_item)
-            spinner.adapter = adapter
-
-            spinner.onItemSelectedListener = object :
-                AdapterView.OnItemSelectedListener {
-                override fun onItemSelected(parent: AdapterView<*>, view: View, position: Int, id: Long) {
-                    Toast.makeText(this@registroAlarma, getString(R.string.selected_item) + " " +
-                            "" + horas[position], Toast.LENGTH_SHORT).show()
-                }
-                override fun onNothingSelected(parent: AdapterView<*>) {
-                }
+                editTextHora.text = Editable.Factory.getInstance().newEditable(SimpleDateFormat("HH:mm").format(cal.time))
             }
+            TimePickerDialog(this, R.style.TimePickerTheme, timeSetListener, cal.get(Calendar.HOUR_OF_DAY), cal.get(
+                Calendar.MINUTE), false).show()
+        }
+    }
+
+    fun configurarEditTextInicio() {
+        val editTextInicio = findViewById(R.id.inicio_periodo) as EditText
+        editTextInicio.setRawInputType(InputType.TYPE_NULL)
+        val cal = Calendar.getInstance()
+
+        editTextInicio.setOnClickListener {
+            val dp = DatePickerDialog.OnDateSetListener { datePicker, year, month, day ->
+                cal.set(Calendar.YEAR, year)
+                cal.set(Calendar.MONTH, month)
+                cal.set(Calendar.DAY_OF_MONTH, day)
+                editTextInicio.text = Editable.Factory.getInstance().newEditable(SimpleDateFormat("yyyy/MM/dd").format(cal.time))
+
+            }
+            DatePickerDialog(this, R.style.TimePickerTheme, dp, cal.get(Calendar.YEAR),
+                cal.get(Calendar.MONTH), cal.get(Calendar.DAY_OF_MONTH)).show()
+        }
+    }
+
+    fun configurarEditTextFin() {
+        val editTextFin = findViewById(R.id.fin_periodo) as EditText
+        editTextFin.setRawInputType(InputType.TYPE_NULL)
+        val cal = Calendar.getInstance()
+
+        editTextFin.setOnClickListener {
+            val dp = DatePickerDialog.OnDateSetListener { datePicker, year, month, day ->
+                cal.set(Calendar.YEAR, year)
+                cal.set(Calendar.MONTH, month)
+                cal.set(Calendar.DAY_OF_MONTH, day)
+                editTextFin.text = Editable.Factory.getInstance().newEditable(SimpleDateFormat("yyyy/MM/dd").format(cal.time))
+
+            }
+            DatePickerDialog(this, R.style.TimePickerTheme, dp, cal.get(Calendar.YEAR),
+                cal.get(Calendar.MONTH), cal.get(Calendar.DAY_OF_MONTH)).show()
         }
     }
 
